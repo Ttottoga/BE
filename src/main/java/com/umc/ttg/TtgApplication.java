068ce19e3d309f8717f1655f3e0406b984f574aa
g00hyun <rhkdgus1417@hufs.ac.kr>
2024-01-05 19:38:50 +0900

    first commit

---
+++ src/main/java/com/umc/ttg/TtgApplication.java
@@ -0,0 +1,13 @@
+package com.umc.ttg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/umc/ttg/TtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.ttg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
