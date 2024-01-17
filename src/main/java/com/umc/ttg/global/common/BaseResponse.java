5a7e903b90828deaee7e29c9078f865b27062ac3
5nam <ohnam90@gmail.com>
2024-01-17 13:24:41 +0900

    ✨ 추가 BaseResponse 1. 필수로 들어가야 하는 필드들은 final 붙임 2. code, message, data 생성자 + code, message 만 넘겨주는 생성자

---
+++ src/main/java/com/umc/ttg/global/common/BaseResponse.java
@@ -0,0 +1,26 @@
+package com.umc.ttg.global.common;
+
+import lombok.Getter;
+
+@Getter
+public class BaseResponse<T> {
+
+    // 필수로 들어가야 하는 요소들은 final 붙임
+    private final int code;
+    private final String message;
+    private T result; // 응답에 데이터가 포함 안될 수도 있으므로 final 뺌
+
+    // code, message, data 모두 넘겨주는 Response
+    public BaseResponse(int status, int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.result = data;
+    }
+
+    // code, message 만 넘겨주는 Response
+    public BaseResponse(int status, int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
