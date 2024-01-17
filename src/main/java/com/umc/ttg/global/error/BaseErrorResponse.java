2553e043650be220aaa98c0e6b1a94517498a6fe
5nam <ohnam90@gmail.com>
2024-01-17 13:49:05 +0900

    🩹 변경 @RequiredArgsConstructor 로 생성자 대체

--- src/main/java/com/umc/ttg/global/error/BaseErrorResponse.java
+++ src/main/java/com/umc/ttg/global/error/BaseErrorResponse.java
@@ -1,17 +1,14 @@
 package com.umc.ttg.global.error;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class BaseErrorResponse {
 
     private final int code;
     private final String message;
-
-    public BaseErrorResponse(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
 
     public BaseErrorResponse(BaseException baseException) {
         this.code = baseException.getCode();
