7c71600c8ffa9c4c7f9348dea507ecde2001a960
5nam <ohnam90@gmail.com>
2024-01-18 14:00:08 +0900

    ♻️ 추가 응답코드 & 에러핸들러 1. RestControllerAdvice 가 @RestController 가 붙은 대상에서 Exception 발생하는 것을 감지하여 Exception 핸들링

--- src/main/java/com/umc/ttg/global/common/ResponseCode.java
+++ src/main/java/com/umc/ttg/global/common/ResponseCode.java
@@ -1,17 +1,34 @@
 package com.umc.ttg.global.common;
 
+import com.umc.ttg.global.error.GeneralException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.function.Predicate;
 
 @Getter
 @RequiredArgsConstructor
 public enum ResponseCode {
-    OK(HttpStatus.OK,"2000", "Ok");
 
+    // 정상 code
+    OK(HttpStatus.OK,"2000", "Ok"),
+
+    // Common Error
+    _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON000", "서버 에러, 관리자에게 문의 바랍니다."),
+    _BAD_REQUEST(HttpStatus.BAD_REQUEST,"COMMON001","잘못된 요청입니다."),
+    _UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"COMMON002","권한이 잘못되었습니다"),
+    _METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON003", "지원하지 않는 Http Method 입니다."),
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON004", "금지된 요청입니다."),
+
+    // Member Error
+    MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "사용자가 없습니다."),
+    NICKNAME_NOT_EXIST(HttpStatus.BAD_REQUEST, "MEMBER4002", "닉네임은 필수 입니다."),
+
+    // Article Error
+    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
@@ -26,5 +43,24 @@ public enum ResponseCode {
         return Optional.ofNullable(message)
                 .filter(Predicate.not(String::isBlank))
                 .orElse(this.getMessage());
+    }
+
+    public static ResponseCode valueOf(HttpStatus httpStatus) {
+        if(httpStatus == null) {
+            throw new GeneralException("HttpStatus is null.");
+        }
+
+        return Arrays.stream(values())
+                .filter(errorCode -> errorCode.getHttpStatus() == httpStatus)
+                .findFirst()
+                .orElseGet(() -> {
+                    if(httpStatus.is4xxClientError()) {
+                        return ResponseCode._BAD_REQUEST;
+                    } else if (httpStatus.is5xxServerError()) {
+                        return ResponseCode._INTERNAL_SERVER_ERROR;
+                    } else {
+                        return ResponseCode.OK;
+                    }
+                });
     }
 }
