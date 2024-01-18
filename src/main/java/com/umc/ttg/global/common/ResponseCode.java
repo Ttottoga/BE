58b643f18d376e156eb15bb4c135d8fe49b0d767
5nam <ohnam90@gmail.com>
2024-01-18 13:38:33 +0900

    ♻️ 생성 API 응답 통일 1. API 응답 통일 코드 BaseResponseDto 생성 : isSuccess, code, message, result 으로 구성 2. 오류 타입 통일 코드 ResponseCode 생성 : 에러 컨벤션은 논의 필요

---
+++ src/main/java/com/umc/ttg/global/common/ResponseCode.java
@@ -0,0 +1,30 @@
+package com.umc.ttg.global.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum ResponseCode {
+    OK(HttpStatus.OK,"2000", "Ok");
+
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    public String getMessage(Throwable e) {
+        return this.getMessage(this.getMessage() + " - " + e.getMessage());
+        // 결과 예시 - "Validation error - Reason why it isn't valid"
+    }
+
+    public String getMessage(String message) {
+        return Optional.ofNullable(message)
+                .filter(Predicate.not(String::isBlank))
+                .orElse(this.getMessage());
+    }
+}
