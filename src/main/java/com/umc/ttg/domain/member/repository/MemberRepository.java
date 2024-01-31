5d1fad74573d6da9e24e7932ba5e1292a3a180aa
nicehcy <nicehcy2@naver.com>
2024-01-31 22:55:49 +0900

    FIX : 머지 충돌 해결

--- src/main/java/com/umc/ttg/domain/member/repository/MemberRepository.java
+++ src/main/java/com/umc/ttg/domain/member/repository/MemberRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    // Member의 정보가 필요해서 임시로 만들었습니다.
+    Optional<Member> findById(Long memberId);
 }
