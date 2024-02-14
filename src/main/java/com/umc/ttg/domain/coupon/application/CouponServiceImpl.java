e37424bad093ebc6967c99907f7af8d5bb481815
YeowonKIM <kkyoo152@gmail.com>
2024-02-15 07:19:37 +0900

    Fix/71 validate member coupon

--- src/main/java/com/umc/ttg/domain/coupon/application/CouponServiceImpl.java
+++ src/main/java/com/umc/ttg/domain/coupon/application/CouponServiceImpl.java
@@ -10,6 +10,7 @@ import com.umc.ttg.domain.member.entity.Member;
 import com.umc.ttg.domain.member.repository.MemberRepository;
 import com.umc.ttg.global.common.BaseResponseDto;
 import com.umc.ttg.global.common.ResponseCode;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -28,31 +29,24 @@ public class CouponServiceImpl implements CouponService {
     private final CouponRepository couponRepository;
 
     @Override
-    public BaseResponseDto<List<CouponResponseDto>> getAllCoupons() {
-        // 로그인 구현 전 임시 멤버 ID
-        Long memberId = 1L;
+    public BaseResponseDto<List<CouponResponseDto>> getAllCoupons(HttpServletRequest request) {
         List<CouponResponseDto> couponResponseDtos = couponRepository
-                .findAllByMemberId(getMember(memberId).getId())
+                .findAllByMemberId(getMemberId(request))
                 .stream().map(CouponResponseDto::of)
                 .collect(Collectors.toList());
         return BaseResponseDto.onSuccess(couponResponseDtos, ResponseCode.OK);
     }
-
     @Override
-    public BaseResponseDto<CouponResponseDto> getCouponDetails(Long couponId) throws IOException, WriterException {
-        // 로그인 구현 전 임시 멤버 ID
-        Long memberId = 1L;
-        Coupon coupon = getCoupon(memberId, couponId);
+    public BaseResponseDto<CouponResponseDto> getCouponDetails(Long couponId, HttpServletRequest request) throws IOException, WriterException {
+        Coupon coupon = getCoupon(couponId, request);
 
         return BaseResponseDto.onSuccess(CouponResponseDto.of(coupon), ResponseCode.OK);
     }
 
     @Override
     @Transactional
-    public BaseResponseDto<String> useCoupon(Long couponId) {
-        // 로그인 구현 전 임시 멤버 ID
-        Long memberId = 1L;
-        Coupon coupon = getCoupon(memberId, couponId);
+    public BaseResponseDto<String> useCoupon(Long couponId, HttpServletRequest request) {
+        Coupon coupon = getCoupon(couponId, request);
 
         // 직원 확인
         if (coupon.getStatusYn().equals('Y')) {
@@ -62,17 +56,17 @@ public class CouponServiceImpl implements CouponService {
         return BaseResponseDto.onSuccess(CouponConverter.convertToCouponUsage(coupon), ResponseCode.OK);
     }
 
-
-    private Member getMember(Long memberId) {
-        Member foundMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> new CouponHandler(ResponseCode.MEMBER_NOT_FOUND));
-        return foundMember;
-    }
-
-    private Coupon getCoupon(Long memberId, Long couponId) {
-        Coupon foundCoupon = couponRepository.findByIdAndMemberId(couponId, getMember(memberId).getId())
+    private Coupon getCoupon(Long couponId, HttpServletRequest request) {
+        Coupon foundCoupon = couponRepository.findByIdAndMemberId(couponId, getMemberId(request))
                 .orElseThrow(() -> new CouponHandler(ResponseCode.COUPON_NOT_FOUND));
         return foundCoupon;
+    }
+
+    private Long getMemberId(HttpServletRequest request) {
+        String name = request.getUserPrincipal().getName();
+        Member member = memberRepository.findByName(name)
+                .orElseThrow(() -> new CouponHandler(ResponseCode.MEMBER_NOT_FOUND));
+        return member.getId();
     }
 
 }
